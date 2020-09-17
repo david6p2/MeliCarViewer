1ff20fb9035121d105daf0f5c5845851ef78ac60
David Andrés Céspedes <ingdavidcespedes@gmail.com>
2020-09-17 00:25:17 -0500

    Move the creation of the UICollectionViewFlowLayout to a UIHelper file

---
+++ MeLiCarViewer/Utilities/UIHelper.swift
@@ -0,0 +1,25 @@
+//
+//  UIHelper.swift
+//  MeLiCarViewer
+//
+//  Created by David A Cespedes R on 9/17/20.
+//  Copyright © 2020 David A Cespedes R. All rights reserved.
+//
+
+import UIKit
+
+struct UIHelper {
+  static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+    let width = view.bounds.width
+    let padding: CGFloat = 12
+    let minimumItemSpacing: CGFloat = 10
+    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+    let itemWidth = availableWidth / 3
+    
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 92)
+    
+    return flowLayout
+  }
+}
--- MeLiCarViewer/Views/CarResultsViewController.swift
+++ MeLiCarViewer/Views/CarResultsViewController.swift
@@ -50,24 +50,10 @@ class CarResultsViewController: UIViewController {
   }
   
   func configureCollectionView() {
-    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
     view.addSubview(collectionView)
     collectionView.backgroundColor = .systemBackground
     collectionView.register(CarViewCell.self, forCellWithReuseIdentifier: CarViewCell.reuseID)
-  }
-  
-  func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-    let width = view.bounds.width
-    let padding: CGFloat = 12
-    let minimumItemSpacing: CGFloat = 10
-    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-    let itemWidth = availableWidth / 3
-    
-    let flowLayout = UICollectionViewFlowLayout()
-    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 92)
-    
-    return flowLayout
   }
   
   func searchPorscheModel() {
