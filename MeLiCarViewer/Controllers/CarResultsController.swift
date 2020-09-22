082e01136f5b6b1bfffaffaa75143b1c16362239
David Andrés Céspedes <ingdavidcespedes@gmail.com>
2020-09-21 22:47:14 -0500

    Add loadCarPicturesInformation request and document network calls

--- MeLiCarViewer/Controllers/CarResultsController.swift
+++ MeLiCarViewer/Controllers/CarResultsController.swift
@@ -17,17 +17,13 @@ class CarResultsController {
   var hasMoreResults = true
   
   private var isFetchInProgress = false
-  var dataLoader: DataLoader
+  private var dataLoader: DataLoader
   
   init(loader: DataLoader = DataLoader(), carModel: CarModel?) {
     self.dataLoader = loader
     self.porscheModelToSearch = carModel
   }
   
-  /// Create a Request to GET the search results of Porsche Cars giving a Model. If  no model is given, it will return all Porsche cars available.
-  /// - Parameters:
-  ///   - model: The model id code for the MCO site in Mercado Libre
-  ///   - completion: Will return a completion closure with the result having the CarModelResult if it succed or the Error if it fails
   func searchPorscheModel(_ model: String?, page: Int = 1, completion: @escaping (Result<CarModelResult?, DCError>) -> Void) {
     guard !isFetchInProgress else {
       return
--- MeLiCarViewer/Models/DataLoaders/DataLoader.swift
+++ MeLiCarViewer/Models/DataLoaders/DataLoader.swift
@@ -16,10 +16,15 @@ class DataLoader {
   let limit = 10
   let offset = 10
   let searchEndpoint = "sites/MCO/search"
+  let pictureDetailsEndpoint = "items/"
   let defaultSession = URLSession(configuration: .default)
   var dataTask: URLSessionDataTask? = nil
   static let noErrorDescription = "No error Description"
   
+  /// Create a Request to GET the search results of Porsche Cars giving a Model. If  no model is given, it will return all Porsche cars available.
+  /// - Parameters:
+  ///   - model: The model id code for the MCO site in Mercado Libre
+  ///   - completion: Will return a completion closure with the result having the CarModelResult if it succed or the Error if it fails
   public func searchResultsForCarModel(_ carModel: String?,
                                        withPage page: Int,
                                        handler: @escaping (Result<CarModelResult, DCError>) -> Void) {
@@ -27,7 +32,7 @@ class DataLoader {
     
     var urlComponents = URLComponents(string: baseEndPoint+searchEndpoint )
     let offset = (page - 1) * limit
-    var queryParameter = "BRAND=56870"
+    var queryParameter = "BRAND=56870" // The Brand ID for Porsche
     if let carModel = carModel, !carModel.isEmpty {
       queryParameter = "MODEL=\(carModel)"
     }
@@ -67,6 +72,57 @@ class DataLoader {
     self.dataTask?.resume()
   }
   
+  
+  /// Create a Request to GET  all the detail information of a given car Id, including the pictures
+  /// - Parameters:
+  ///   - carId: item id (the car id in this case). Ex MCO578263412
+  ///   - handler: Will return a handler closure with the result having the CarPicturesInformation if it succed or the Error if it fails
+  public func loadCarPicturesInformation(withCarId carId: String, handler: @escaping (Result<CarPicturesInformation, DCError>) -> Void) {
+    dataTask?.cancel()
+    
+    let urlComponents = URLComponents(string: baseEndPoint + pictureDetailsEndpoint + carId)
+    
+    guard let url = urlComponents?.url else {
+      handler(.failure(DCError(type: .invalidCarModel, errorInfo: "The search URL was not valid. Check your URL. (\(urlComponents ?? URLComponents(string:"N/A")!)")))
+      return
+    }
+    
+    self.dataTask = defaultSession.dataTask(with: url, completionHandler: { [weak self] (data, response, error) in
+      defer { self?.dataTask = nil }
+      
+      if let error = error {
+        handler(.failure(DCError(type: .unableToComplete, errorInfo: error.localizedDescription)))
+        return
+      }
+      
+      guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        handler(.failure(DCError(type: .invalidResponse, errorInfo: error?.localizedDescription ?? Self.noErrorDescription)))
+        return
+      }
+      
+      guard let data = data else {
+        handler(.failure(DCError(type: .invalidData, errorInfo: error?.localizedDescription ?? Self.noErrorDescription)))
+        return
+      }
+      
+      do {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let decodedResponse = try decoder.decode(CarPicturesInformation.self, from: data)
+        handler(.success(decodedResponse))
+      } catch {
+        let errorDesc = error as NSError
+        handler(.failure(DCError(type: .unableToDecode, errorInfo: errorDesc.debugDescription)))
+      }
+    })
+    self.dataTask?.resume()
+  }
+  
+  
+  /// Load the car Models from a JSON file
+  /// - Parameters:
+  ///   - fileName: the filename of the JSON where the car models are stored
+  ///   - handler: Will return a handler closure with the result having the CarModels in an array if it succed or the Error if it fails
   private func loadCarModelJSON(fileName: String, handler: @escaping (Result<[CarModel], DCError>) -> Void) {
     if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
       do {
@@ -82,6 +138,9 @@ class DataLoader {
     }
   }
   
+  
+  /// Call to get the Porsche Models from a JSON file called "PorscheModels"
+  /// - Parameter handler: Will return a handler closure with the result having the CarModels result in an array if it succed or the Error if it fails
   public func getPorscheModels(handler: @escaping (Result<[CarModel], DCError>) -> Void) {
     // Loading from JSON File
     self.loadCarModelJSON(fileName: "PorscheModels") { (result) in
@@ -94,6 +153,11 @@ class DataLoader {
     }
   }
   
+  
+  /// Create a Request to GET an image from the passed urlString
+  /// - Parameters:
+  ///   - urlString: The URL String of the image
+  ///   - handler: Will return a handler closure with the result having the requested UIImage if it succed or the Error if it fails
   public func downloadImage(from urlString: String, handler: @escaping (Result<UIImage, DCError>) -> Void) {
     let cacheKey = NSString(string: urlString)
     if let image = Self.cache.object(forKey: cacheKey) {
