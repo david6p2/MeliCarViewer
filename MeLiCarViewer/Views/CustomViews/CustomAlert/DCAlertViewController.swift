a2f0d431773538c7eab96e1e66aa11b84a3dd1b5
David Andrés Céspedes <ingdavidcespedes@gmail.com>
2020-09-24 10:52:32 -0500

    Create DCAlertContainerView and refactor DCAlertVC

--- MeLiCarViewer/Views/CustomViews/CustomAlert/DCAlertViewController.swift
+++ MeLiCarViewer/Views/CustomViews/CustomAlert/DCAlertViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class DCAlertViewController: UIViewController {
   
-  let containerView = UIView()
+  let containerView = DCAlertContainerView()
   let titleLabel = DCTitleLabel(textAlignment: .center, fontSize: 20)
   let messageLabel = DCBodyLabel(textAlignment: .center)
   let actionButton = DCButton(backgroundColor: .systemPink, title: "OK")
@@ -34,7 +34,7 @@ class DCAlertViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+    view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
     configureContainerView()
     configureTitleLabel()
     configureActionButton()
@@ -43,11 +43,6 @@ class DCAlertViewController: UIViewController {
   
   func configureContainerView() {
     view.addSubview(containerView)
-    containerView.backgroundColor = .systemBackground
-    containerView.layer.cornerRadius = 16
-    containerView.layer.borderWidth = 2
-    containerView.layer.borderColor = UIColor.white.cgColor
-    containerView.translatesAutoresizingMaskIntoConstraints = false
     
     NSLayoutConstraint.activate([
       containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
---
+++ MeLiCarViewer/Views/CustomViews/CustomAlert/Views/DCAlertContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  DCAlertContainerView.swift
+//  MeLiCarViewer
+//
+//  Created by David A Cespedes R on 9/24/20.
+//  Copyright © 2020 David A Cespedes R. All rights reserved.
+//
+
+import UIKit
+
+class DCAlertContainerView: UIView {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func configure() {
+    backgroundColor = .systemBackground
+    layer.cornerRadius = 16
+    layer.borderWidth = 2
+    layer.borderColor = UIColor.white.cgColor
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+}
